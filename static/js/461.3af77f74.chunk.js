610426b80f5640eeb221cb6f4132f1da07e1270d
Abhishek Rajput <abhishekrajput676@gmail.com>
2025-01-26 07:48:41 +0000

    Updates

---
+++ static/js/461.3af77f74.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_practice_application=self.webpackChunkreact_practice_application||[]).push([[461],{461:(e,c,r)=>{r.r(c),r.d(c,{default:()=>i});r(43);var a=r(579);const i=()=>(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{children:"User Profile"}),(0,a.jsx)("p",{children:"Welcome to the user profile page."})]})}}]);
+//# sourceMappingURL=461.3af77f74.chunk.js.map
